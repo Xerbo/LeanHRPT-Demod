82d6bf4edd98016012bb50a3208674c83ca5b35d
Xerbo <xerbo@protonmail.com>
2022-05-14 16:26:14 +0100

    Fix debian builds see Xerbo/LeanHRPT-Decode@da4f829

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,7 @@ make install
 
 cd /root/LeanHRPT-Demod
 mkdir build && cd build
+git config --global --add safe.directory /root/LeanHRPT-Demod
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release ..
 make -j4
 make package
